28b11e85e7683f46fe18631d731c7a8da3883aac
Steven-N-Hart <steven.n.hart@gmail.com>
2020-08-17 10:44:52 -0500

    trying to fix requirements.txt failures in TRAVIS build

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as requirements:
+with open('./requirements.txt') as requirements:
     req = requirements.read()
 
 requirements = [req]
