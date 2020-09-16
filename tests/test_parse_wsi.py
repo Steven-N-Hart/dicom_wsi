8ca316737c7e9add6afe614f014541048f2b266c
Steven-N-Hart <steven.n.hart@gmail.com>
2020-09-16 08:39:11 -0500

    pytest passing

--- tests/test_parse_wsi.py
+++ tests/test_parse_wsi.py
@@ -16,8 +16,8 @@ def test_get_wsi():
     cfg = load(open(base_yaml), Loader=BaseLoader)
 
     wsi_fn_retuned = cfg.get('General')
-    wsi_fn_expected = {'WSIFile': '../tests/CMU-1-JP2K-33005.svs', 'OutFilePrefix': './tests/output',
-                       'NumberOfLevels': '7', 'OrgUIDRoot': '1.2.840.113713.15.', 'WSIBrand': 'aperio_svs',
+    wsi_fn_expected = {'WSIFile': 'tests/CMU-1-JP2K-33005.svs', 'OutFilePrefix': './tests/output',
+                       'NumberOfLevels': '7', 'OrgUIDRoot': '1.2.840.113713.15.',
                        'FrameSize': '500', 'MaxFrames': '500', 'ImageFormat': '.jpg', 'CompressionAmount': '90',
                        'background_range': '80', 'threshold': '0.5', 'Annotations': './tests/CMU-1-JP2K-33005.xml'}
     dict_wsi_fn_retuned = json.dumps(wsi_fn_retuned, sort_keys=True)
